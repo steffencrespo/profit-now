58601cb109619d53f1e4c13f88420d7ab647b289
Leonardo Steffen <steffencrespo@hotmail.com>
2013-02-23 22:06:35 -0300

    adding field names to the string file

--- gen/com/example/profitnow/R.java
+++ gen/com/example/profitnow/R.java
@@ -35,8 +35,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int calculate_button=0x7f040008;
+        public static final int current_price=0x7f040006;
+        public static final int final_target=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int percent_profit=0x7f040005;
+        public static final int profit_now=0x7f040007;
+        public static final int purchase_price=0x7f040003;
     }
     public static final class style {
         /** 
